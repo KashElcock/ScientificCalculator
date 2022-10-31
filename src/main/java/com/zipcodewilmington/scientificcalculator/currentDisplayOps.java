4a5b4cada934b4066f0631cd52666cffc411d21f
Calvin <calvin@Zipcoders-MacBook-Pro.local>
2022-10-30 23:04:10 -0400

    edited div statements & added inverse & switchsign methods

--- src/main/java/com/zipcodewilmington/scientificcalculator/currentDisplayOps.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/currentDisplayOps.java
@@ -13,10 +13,14 @@ public class currentDisplayOps {
                 "               \n2. Subtraction " +
                 "               \n3. Multiplication " +
                 "               \n4. Division " +
-                "               \n5. Function(Main) Menu " +
-                "               \n6. Exit App " +
+                "               \n5. Squared " +
+                "               \n6. Square Root " +
+                "               \n7. Inverse " +
+                "               \n8. Switch Sign" +
+                "               \n9. Function(Main) Menu " +
+                "               \n10. Exit App " +
                 "               \nCurrent Display: %.2f" +
-                "               \n7. Clear Display", currentDisplay);
+                "               \n11. Clear Display", currentDisplay);
         int operationInput = Console.getIntegerInput("\nEnter the corresponding number: ");
         if (Objects.equals(operationInput, 1)) { //addition
             num2 = Console.getDoubleInput("\n- A D D I T I O N - \nEnter the second number: ");
@@ -32,16 +36,37 @@ public class currentDisplayOps {
             Console.println("%,.0f * %,.0f = %,.0f", num1, num2, currentDisplay);
         } else if (Objects.equals(operationInput, 4)) { //division
             num2 = Console.getDoubleInput("\n- D I V I S I O N - \nEnter the second number: ");
-            currentDisplay = mathOps.division(num1, num2);
-            Console.println("%,.0f / %,.0f = %,.2f", num1, num2, currentDisplay);
             if (num2 == 0) {
                 Console.println("[ERROR] Cannot divide by 0.");
+            } else {
+                currentDisplay = mathOps.division(num1, num2);
+                Console.println("%,.0f / %,.0f = %,.2f", num1, num2, currentDisplay);
             }
-        } else if (Objects.equals(operationInput, 6)) { //Exit
+        } else if (Objects.equals(operationInput, 5)) { //squared
+            Console.println("\n- S Q U A R E D -");
+            double holder = currentDisplay;
+            currentDisplay = mathOps.getSquare(currentDisplay);
+            Console.println("%,.0f^2 = %,.0f", holder, currentDisplay);
+        } else if (Objects.equals(operationInput, 6)) { //square root
+            Console.println("\n- S Q - R O O T -");
+            double holder = currentDisplay;
+            currentDisplay = mathOps.getSquareRoot(currentDisplay);
+            Console.println("Square Root of %,.0f = %,.0f", holder, currentDisplay);
+        } else if (Objects.equals(operationInput, 7)) { //7. Invserse
+            Console.println("\n- I N V E R S E -");
+            double holder = currentDisplay;
+            currentDisplay = mathOps.getInverse(currentDisplay);
+            Console.println("Inverse of %,.0f = %s", holder, currentDisplay);
+        } else if (Objects.equals(operationInput, 8)) { //8. Switch Sign
+            Console.println("\n- S W I T C H - S I G N -");
+            double holder = currentDisplay;
+            currentDisplay = mathOps.getSwitchSign(currentDisplay);
+            Console.println("Switching the sign of %,.0f = %,.0f", holder, currentDisplay);
+        } else if (Objects.equals(operationInput, 10)) { //Exit
             Console.println("-- Closing application --");
             //exit = true;
             System.exit(0);
-        } else if (Objects.equals(operationInput, 7)) { //Clear Display
+        } else if (Objects.equals(operationInput, 11)) { //Clear Display
             currentDisplay = 0.00;
             currentDisplayOps.opsDisplayValue(currentDisplay);
             Console.println("[NOTICE] Current display value has been reset.");
